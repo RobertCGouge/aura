28d150c757160b84182b4d35faa904488283c3c5
Robert Gouge <robert.c.gouge@gmail.com>
2025-10-10 15:16:39 -0500

    finish 91

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,25 @@
+// Copyright Robert Gouge 2025
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHud.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAuraHUD* AuraHud = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return AuraHud->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
