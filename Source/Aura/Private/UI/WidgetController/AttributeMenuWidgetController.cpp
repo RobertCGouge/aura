c5b8871cc6e22c25e35a444220c3bc39d406d0a2
Gouge, Robert <robert.gouge@cgi.com>
2025-10-10 11:45:30 -0500

    finish 90

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+ // Copyright Robert Gouge 2025
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+ {
+	
+ }
+
+ void UAttributeMenuWidgetController::BroadcastInitialValues()
+ {
+	
+ }
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+// Copyright Robert Gouge 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadcastInitialValues() override;
+};
